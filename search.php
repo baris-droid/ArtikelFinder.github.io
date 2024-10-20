ae08ce2bff578b5aca54a4ab4efae6825a519f33
HerrLavrencium <patatesking91@gmail.com>
2024-10-20 13:09:58 +0300

    update

---
+++ search.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Gelen veriyi al
+    $input = json_decode(file_get_contents('php://input'), true);
+    $query = strtolower($input['word']);
+
+    // JSON dosyasını oku
+    $jsonFilePath = 'words.json';
+    $data = file_get_contents($jsonFilePath);
+    $words = json_decode($data, true);
+
+    // Kelimeyi bul ve searchCount artır
+    $found = false;
+    foreach ($words as &$wordObj) {
+        if (strtolower($wordObj['word']) === $query) {
+            $wordObj['searchCount'] += 1;
+            $found = true;
+            $response = [
+                'word' => $wordObj['word'],
+                'article' => $wordObj['article'],
+                'searchCount' => $wordObj['searchCount']
+            ];
+            break;
+        }
+    }
+
+    if ($found) {
+        // Güncellenmiş veriyi JSON dosyasına yaz
+        file_put_contents($jsonFilePath, json_encode($words, JSON_PRETTY_PRINT));
+        echo json_encode($response);
+    } else {
+        echo json_encode(['message' => 'Kelime bulunamadı!']);
+    }
+} else {
+    echo json_encode(['message' => 'Geçersiz istek!']);
+}
+?>
